fda30e4d096a85885519bc3788b5d6e97bd7bcaa
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-03-14 14:33:12 +0100

    Edit Ex2

--- week1/react-exercises/src/components/GuaranteeList.js
+++ week1/react-exercises/src/components/GuaranteeList.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { useState } from 'react';
 import Guarantee from './Guarantee';
 import uuid from 'react-uuid';
 import freeShipping from '../images/f-delivery.png';
 import coin from '../images/coin.png';
 import chat from '../images/chat.png';
 
+const guaranteeItems= [
+    {
+        id: uuid(),
+        image: freeShipping,
+        title: 'Free Shipping',
+        description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
+    },
+    {
+        id: uuid(),
+        image: coin,
+        title: '100% Money Back',
+        description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
+    },
+    {
+        id: uuid(),
+        image: chat,
+        title: 'Online Support 24/7',
+        description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
+    }
+];
+
 
 const GuaranteeList = () => {
-    const [guaranteeItems, setGuaranteeItems] = useState([
-                {
-                    id: uuid(),
-                    image: freeShipping,
-                    title: 'Free Shipping',
-                    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
-                },
-                {
-                    id: uuid(),
-                    image: coin,
-                    title: '100% Money Back',
-                    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
-                },
-                {
-                    id: uuid(),
-                    image: chat,
-                    title: 'Online Support 24/7',
-                    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,Lorem ipsum dolor sit amet, percipit repudiande dolor sit amet lerm curd vom repudiande...'
-                }
-            ]
-        );
+    
     return (
         <div className='exerciseBlock'>
             <h2>Exercise 2</h2>
