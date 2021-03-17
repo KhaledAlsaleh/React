b952c7070f1a42c6532ea63365ceb69aea3813c1
Khaled Alsaleh <khalsalehsyria1@gmail.com>
2021-03-17 12:17:32 +0100

    Build & Deploy Exercises

--- week2/react-exercises/src/components/Ex 1-New friend on demand/FriendProfile.js
+++ week2/react-exercises/src/components/Ex 1-New friend on demand/FriendProfile.js
@@ -5,7 +5,7 @@ const FriendProfile = ({props}) => {
             <ul id='friendInfo'>
                 <li><span>Full Name: </span>{props.name.title} {props.name.first} {props.name.last}</li>
                 <li><span>Country & City: </span>{props.location.country}, {props.location.city}</li>
-                <li><span>Address: </span>{props.location.street.name} {props.location.street.number}, {props.location.postcode}</li>
+                <li><span>Address: </span>{props.location.street.name} {props.location.street.number},  {props.location.postcode}</li>
                 <li><span>Email Address: </span>{props.email}</li>
                 <li><span>Phone Number: </span>{props.phone}</li>
             </ul>
